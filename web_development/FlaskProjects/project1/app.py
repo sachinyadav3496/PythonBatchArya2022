146aaadee54b08f44b653e8a2656756a30b4a37d
sachinyadav3496 <sachinyadav3496@gmail.com>
2022-08-03 21:21:51 +0530

    Flask Powered Web Page!

---
+++ web_development/FlaskProjects/project1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# initlizing flask WSGI application
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # host=localhost, port=5000
